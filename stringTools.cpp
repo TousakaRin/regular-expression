13919636cc203f14499a7a458696be288d198744
Soultz <lhestz@163.com>
2016-01-05 17:13:14 +0800

    add stringTools.

---
+++ stringTools.cpp
@@ -0,0 +1,32 @@
+#include "stringTools.h"
+#include <algorithm>
+
+using namespace rgx;
+using namespace std;
+
+string int2string(int i) {
+    string s;
+    if (i == 0) {
+        return "0";
+    }
+    if (i < 0) {
+        i = -i;
+        s.push_back('-');
+    }
+    string tmp;
+    while(i != 0) {
+        tmp.push_back(i % 10);
+        i /= 10;
+    }
+    reverse(tmp.begin(), tmp.end());
+    s += tmp;
+    return s;
+} 
+
+string bool2string(bool b) {
+    if (b) {
+        return "true";
+    } else {
+        return "false";
+    }
+}
---
+++ stringTools.h
@@ -0,0 +1,13 @@
+#ifndef _STRING_TOOLS_
+#define _STRING_TOOLS_
+
+#include <string>
+
+namespace rgx {
+
+std::string int2string(int);
+std::string bool2string(bool);
+
+
+}
+#endif
